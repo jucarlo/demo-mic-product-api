0408e94351ebb2b3b02252f71053716247270d78
JuanCarlos Lopez <jucarlo86@gmail.com>
2026-07-27 16:58:36 -0600

    Project File Structure

---
+++ src/test/java/dev/jclp/demo/product_api/ProductApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jclp.demo.product_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
