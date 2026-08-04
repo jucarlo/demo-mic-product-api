e955d63037e7e3ea35a532ce109b13875a94f309
JuanCarlos Lopez <jucarlo86@gmail.com>
2026-08-04 13:46:43 -0600

    33 prepare correlation id

--- src/main/java/dev/jclp/demo/product_api/services/ProductCommandServiceImpl.java
+++ src/main/java/dev/jclp/demo/product_api/services/ProductCommandServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.stream.function.StreamBridge;
 import dev.jclp.demo.product_api.model.Command;
 import dev.jclp.demo.product_api.model.dto.ProductDto;
 import org.springframework.cloud.stream.function.StreamBridge;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -20,7 +22,14 @@ public class ProductCommandServiceImpl implements ProductCommandService {
     @Override
     public void sendCreate(ProductDto productDto) {
         Command<ProductDto> command = new Command<>("CREATE", null, productDto);
-        boolean sent = streamBridge.send("commands-out-0", command);
+
+        String correlationId = java.util.UUID.randomUUID().toString();
+
+        Message<Command<ProductDto>> message = MessageBuilder.withPayload(command)
+                .setHeader("correlationId", correlationId)
+                .build();
+
+        boolean sent = streamBridge.send("commands-out-0", message);
         if (!sent) {
             throw new IllegalStateException("Failed sending kafka command");
         }
